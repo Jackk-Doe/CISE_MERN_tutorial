e45f1c349a69c36ef718ffd1baf8f60ab3e929ea
Jackk-Doe <dodo-stv@hotmail.com>
2022-03-30 17:26:38 +1300

    Finish Backend part
    - Updated app.js to work with Book API
    - Installed 'cors' package

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const connectDB = require('./config/db')
+let cors = require('cors')
+
+// Routes
+const books = require(('./routes/api/books'))
 
 const app = express()
 
 // Connect Database
 connectDB()
 
+// cors
+app.use(cors({ origin: true, credentials: true}))
+
+// Init Middleware
+app.use(express.json({ extended: false }))
+
 // First Path
-app.get('/', (req, res) => res.send('Hello world!'));
+// app.get('/', (req, res) => res.send('Hello world!'));
+
+// Use Routes
+app.use('/api/books', books)
 
 // Set Port number to 8082
 const port = process.env.PORT || 8082;
